b0424a779c7698073e185ab7f87ef2b39b4319da
rohan <purohit.rohan@gmail.com>
2016-09-04 19:36:47 +0530

    Selinux: Permissive
    Change-Id: Ic73b0e5d890e9ffe8465143d2082c964d9abf9ad

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -77,6 +77,7 @@ WITH_DEXPREOPT_BOOT_IMG_ONLY ?= true
 BOARD_DTBTOOL_ARGS                 := -2
 BOARD_MKBOOTIMG_ARGS               := --ramdisk_offset 0x02000000 --tags_offset 0x01E00000
 BOARD_KERNEL_CMDLINE               := console=none androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1
+BOARD_KERNEL_CMDLINE               += androidboot.selinux=permissive
 BOARD_KERNEL_SEPARATED_DT          := true
 BOARD_KERNEL_BASE                  := 0x80000000
 BOARD_KERNEL_PAGESIZE              := 2048
